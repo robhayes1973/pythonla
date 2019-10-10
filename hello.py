8b5226323e13195044cf2a6e93ca90cc052edec5
Robert Hayes <robhayes1973@gmail.com>
2019-10-10 09:26:49 +0300

    Updates to hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
+# This is the hello world starter program.
 print("Hellow world")
+print(1 + 1) #this is math
+print("Ha" * 4)
+"""
+This is a starter program for learning python.
+This part of the file is to show how to do block comments.
+I think I get it.
+"""
